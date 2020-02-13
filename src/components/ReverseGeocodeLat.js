8a049f39b7ab8b7a885c1828e609f1f33a01524f
unknown <gabrielwonggg@gmail.com>
2020-02-13 18:16:48 +0800

    Added React project files

---
+++ src/components/ReverseGeocodeLat.js
@@ -0,0 +1,186 @@
+
+function ReverseGeocodeLat(lat,long,array) {
+         
+        switch((lat,long)) {
+          case (1.323604823,103.8587802):
+            return "7 St George's Lane,Kallang,south," + array;
+          case (1.34355015,103.8601984):
+            return "Braddell Flyover,Serangoon,central," + array;
+          case (1.28569398886979,103.837524510188):
+            return "Chin Swee Rd,Bukit Merah,south," + array;
+          case (1.375925022,103.8587986):
+            return "Ang Mo Kio North Flyover,Ang Mo Kio,central," + array;
+          case (1.38861,103.85806):
+            return "68 Sunrise Ave,Seletar,north," + array;
+          case (1.28036584335876,103.830451146503):
+            return "127A Kim Tian Rd,Bukit Merah,south," + array;
+          case (1.31384231654635,103.845603032574):
+            return "8 Norfolk Rd,Novena,central," + array;
+          case (1.35296,103.85719):
+            return "Ang Mo Kio South Flyover,Bishan,central," + array;
+          case (1.447023728,103.7716543):
+            return "Woodland Checkpoint (1),Woodlands,north," + array;
+          case (1.445554109,103.7683397):
+            return "Woodland Checkpoint (2),Woodlands,north," + array;
+          case (1.348697862,103.6350413):
+            return "Tuas Checkpoint (1),Tuas,west," + array;
+          case (1.32153,103.75273):
+            return "101 West Coast Vale,Clementi,west," + array;
+          case (1.347645829,103.6366955):
+            return "Tuas Checkpoint (2),Tuas,west," + array;
+          case (1.29531332,103.871146):
+            return "187 Tg Rhu Rd,City,south," + array;
+          case (1.319541067,103.8785627):
+            return "56 Sims Dr,Geylang,east," + array;
+          case (1.323957439,103.8728576):
+            return "8 Kallang Sector,Kallang,south," + array;
+          case (1.319535712,103.8750668):
+            return "KPE (Geylang),Geylang,east," + array;
+          case (1.363519886,103.905394):
+            return "Defu Flyover,Hougang,east," + array;
+          case (1.357098686,103.902042):
+            return "25 Defu Lane 3,Paya Lebar,east," + array;
+          case (1.27414394350065,103.851316802547):
+            return "11A Straits Boulevard,City,south," + array;
+          case (1.27135090682664,103.861828440597):
+            return "Marina Sth Pier,City,south," + array;
+          case (1.27066408655104,103.856977943394):
+            return "71 Marina Coastal Dr,City,south," + array;
+          case (1.29409891409364,103.876056196568):
+            return "500 ECP,City,south," + array;
+          case (1.2752977149006,103.866390381759):
+            return "MCE,City,south," + array;
+          case (1.35047790791386,103.791033581325):
+            return "BKE (Central Water Catchment),Central Water Catchment,central," + array;
+          case (1.429588536,103.769311):
+            return "BKE (Woodland Ave 3),Woodlands,north," + array;
+          case (1.36728572,103.7794698):
+            return "BKE (Dairy Farm Rd),Bukit Panjang,central," + array;
+          case (1.414142,103.771168):
+            return "BKE (Mandai Rd),Mandai,north," + array;
+          case (1.3983,103.774247):
+            return "BKE (Mandai Range),Mandai,north," + array;
+          case (1.3865,103.7747):
+            return "467 Segar Rd,Bukit Panjang,central," + array;
+          case (1.33831,103.98032):
+            return "Airport Blvd,Changi,east," + array;
+          case (1.32743,103.97383):
+            return "ECP (Tanah Merah Flyover),Changi,east," + array;
+          case (1.30145145166066,103.910596320237):
+            return "Marine Parade Flyover,Marine Parade,central," + array;
+          case (1.297512569,103.8983019):
+            return "2 Tanjong Katong Rd South,Marine Parade,central," + array;
+          case (1.29565733262976,103.885283049309):
+            return "45 Marina East Drive,Marine Parade,central," + array;
+          case (1.29158484,103.8615987):
+            return "Marina Ctr Ter,City,south," + array;
+          case (1.2871,103.79633):
+            return "2 Warwick Rd,Queenstown,central," + array;
+          case (1.27237,103.8324):
+            return "43 Keppel Rd,Bukit Merah,central," + array;
+          case (1.27877,103.82375):
+            return "AYE (Lower Delta Rd),Bukit Merah,central," + array;
+          case (1.32618,103.73028):
+            return "24 Jln Papan,Jurong West,west," + array;
+          case (1.29792,103.78205):
+            return "39 Dover Rd,Queenstown,central," + array;
+          case (1.33344648135658,103.652700847056):
+            return "37 Tuas Ave 8,Tuas,west," + array;
+          case (1.29939,103.7799):
+            return "Opp SIT Dover,Queenstown,central," + array;
+          case (1.312019,103.763002):
+            return "427 Clementi Ave 3,Clementi,west," + array;
+          case (1.341244001,103.6439134):
+            return "AYE (Tuas),Tuas,west," + array;
+          case (1.31023,103.76438):
+            return "12 W Coast Walk,Clementi,west," + array;
+          case (1.32227,103.67453):
+            return "151 Gul Cir,Pioneer,west," + array;
+          case (1.25999999687243,103.823611110166):
+            return "6 Sentosa Gateway (1),Sentosa,south," + array;
+          case (1.26027777363278,103.823888890049):
+            return "6 Sentosa Gateway (2),Sentosa,south," + array;
+          case (1.3309693,103.9168616):
+            return "PIE (Bedok North),Bedok,east," + array;
+          case (1.326024822,103.905625):
+            return "PIE (Ubi Ave 1),Geylang,east," + array;
+          case (1.322875288,103.8910793):
+            return "01-04 Paya Lebar Rd,Paya Lebar,east," + array;
+          case (1.32036078126842,103.877174116489):
+            return "60 Jln Kolam Ayer,Geylang,east," + array;
+          case (1.328171608,103.8685191):
+            return "PIE (Macperson Rd),Toa Payoh,central," + array;
+          case (1.329334,103.858222):
+            return "PIE (Kallang River),Toa Payoh,central," + array;
+          case (1.328899,103.84121):
+            return "Thomson Flyover,Novena,central," + array;
+          case (1.32657403632366,103.826857295633):
+            return "230 Whitley Rd,Novena,central," + array;
+          case (1.332124,103.81768):
+            return "PIE (Adam Rd),Bukit Timah,central," + array;
+          case (1.349428893,103.7952799):
+            return "PIE (Champion Golf),Bukit Timah,central," + array;
+          case (1.345996,103.69016):
+            return "Nanyang Flyover,Jalan Bahar,west," + array;
+          case (1.344205,103.78577):
+            return "15 Binjai Rise,Bukit Timah,central," + array;
+          case (1.33771,103.977827):
+            return "Changi Flyover,Changi,east," + array;
+          case (1.332691,103.770278):
+            return "43 Eng Kong Dr,Bukit Batok,west," + array;
+          case (1.340298,103.945652):
+            return "23 Tampines Street 92,Tampines,east," + array;
+          case (1.361742,103.703341):
+            return "Tengah Flyover,Tengah,west," + array;
+          case (1.356299,103.716071):
+            return "Hong Kah Flyover,Tengah,west," + array;
+          case (1.322893,103.663505):
+            return "Tuas Flyover,Tuas,west," + array;
+          case (1.354245,103.963782):
+            return "110Q Jln Mayaanam,Pasir Ris,east," + array;
+          case (1.37704704,103.92946983):
+            return "TPE (Pasir Ris Dr 12),Pasir Ris,east," + array;
+          case (1.37988658,103.92009174):
+            return "3 Tampines Industrial Drive,Tampines,east," + array;
+          case (1.38432741,103.91585701):
+            return "TPE (Tampines Rd),Paya Lebar,east," + array;
+          case (1.39559294,103.90515712):
+            return "Bef Punggol Road,Punggol,north," + array;
+          case (1.40002575,103.85702534):
+            return "TPE (Seletar West Link),Seletar,north," + array;
+          case (1.39748842,103.85400467):
+            return "Seletar Flyover,Seletar,north," + array;
+          case (1.38647,103.74143):
+            return "KJE (Choa Chu Kang Way),Choa Chu Kang,west," + array;
+          case (1.39059,103.7717):
+            return "480A Segar Rd,Bukit Panjang,central," + array;
+          case (1.3899,103.74843):
+            return "1 Choa Chu Kang Street 53,Choa Chu Kang,west," + array;
+          case (1.3664,103.70899):
+            return "KJE (Old Choa Chu Kang Rd),Tengah,west," + array;
+          case (1.39466333,103.83474601):
+            return "Lentor Flyover,Yishun,north," + array;
+          case (1.39474081,103.81797086):
+            return "SLE (Upper Thomson Rd),Mandai,north," + array;
+          case (1.422857,103.773005):
+            return "Woodlands South Flyover,Woodlands,north," + array;
+          case (1.42214311,103.79542062):
+            return "Ulu Sembawang Flyover,Woodlands,north," + array;
+          case (1.42627712,103.78716637):
+            return "3 Champions Way,Woodlands,north," + array;
+          case (1.41270056,103.80642712):
+            return "Mandai Crematorium,Mandai,north," + array;
+          case (1.32814722194857,103.862203282048):
+            return "23 Mar Thoma Rd,Kallang,south," + array;
+          case (1.2958550156561,103.880314665981):
+            return "ECP (Tanjong Rhu Flyover),Marine Parade,central," + array;
+          case (1.309330837,103.9350504):
+            return "Laguna Flyover,Bedok,east," + array;
+          default:
+            return 'Not Available,NA,NA,-1'
+        }
+    
+}
+
+export default ReverseGeocodeLat
+
